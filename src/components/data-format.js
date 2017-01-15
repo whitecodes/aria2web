87032c883c57d1f7768cc071ec07716977e2938f
whitecodes <whitemailg@gmail.com>
2017-01-15 21:17:33 +0800

    remove useless files

--- src/components/data-format.js
+++ src/components/data-format.js
@@ -2,9 +2,9 @@
 export default {
     speedFormat: function(speed){
         var speedBps = speed * 1;
-        if(speedBps > 1000) {
+        if(speedBps > 1024) {
             var speedKbps = (speedBps / 1024).toFixed(2);
-            if(speedKbps > 1000) {
+            if(speedKbps > 1024) {
                 var speedMbps = (speedKbps / 1024).toFixed(2);
                 return speedMbps + 'M';
             }else{
